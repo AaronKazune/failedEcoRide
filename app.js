f01ee98d95522f82edf4676fa0185c427b7c8b2f
Aaron <Arynhd@gmail.com>
2023-07-04 12:16:32 +0800

    added test, addes route for stylesheet, allow css

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set("port",process.env.PORT || 3000);
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname + 'public')));
 
 //app.set('view engine', 'html');
 
