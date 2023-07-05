463c7663f15730614907ea794f431cba1f4b9231
Aaron <Arynhd@gmail.com>
2023-07-05 16:42:41 +0800

    app broke for some reason fixed it idk how

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -2,6 +2,6 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get('/test', (req, res) => res.send('API test route works'));
+
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-router.use("/api", require("./users"));
+
 router.use("/api", require("./admin"));
 
 module.exports = router;
